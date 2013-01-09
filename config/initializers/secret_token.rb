f4a40d77efccb5bc142eb505a98515b242c881cf
Ahmad Saifuddin <superdeen@brainstorm.pro>
2013-01-09 23:37:55 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveDummy::Application.config.secret_token = 'ba86863499176e626ec6d33f0a3075d0a52f5a73631b2297a67e511c537cfcb1f9c05f44e4cdb06e2a82412d26c195c0fb9b77b6d2aa60c88c2b50d84e4adf61'
